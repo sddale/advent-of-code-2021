a4ba03a97bd05612832f871cd600746af8bea053
sddale <seth.d.dale@gmail.com>
2021-12-21 22:54:40 -0500

    TIL inner_product

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -2,31 +2,28 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <string>
 #include <vector>
 
-//Prompt: https://adventofcode.com/2021/day/1
+// Prompt: https://adventofcode.com/2021/day/1
 
-unsigned int part_one() {
-  std::ifstream fin("input1.txt");
-  std::istream_iterator<unsigned int> fit(fin);
-  unsigned int prev = INT32_MAX;
-  return std::count_if(
-      fit, std::istream_iterator<unsigned int>(),
-      [&prev](unsigned int i) { return (std::exchange(prev, i) < i); });
+int part_one() {
+  std::ifstream fin("../input1.txt");
+  std::istream_iterator<int> fit(fin);
+  std::vector<int> v;
+  std::move(fit, std::istream_iterator<int>(), std::back_inserter(v));
+  return std::inner_product(std::next(v.begin(), 1), v.end(), v.begin(), 0,
+                            std::plus<>(), std::greater<>());
 }
-unsigned int part_two() {
-  std::ifstream fin("input2.txt");
-  std::istream_iterator<unsigned int> fit(fin);
-  unsigned int prevprev = *fit++;
-  unsigned int prev = *fit++;
-  return std::count_if(
-      fit, std::istream_iterator<unsigned int>(),
-      [&, curr = 0, sum = INT32_MAX](const unsigned int i) mutable -> bool {
-        prevprev = std::exchange(prev, std::exchange(curr, i));
-        return (std::exchange(sum, prevprev + prev + curr) <
-                prevprev + prev + curr);
-      });
+int part_two() {
+  std::ifstream fin("../input1.txt");
+  std::istream_iterator<int> fit(fin);
+  std::vector<int> v;
+  std::move(fit, std::istream_iterator<int>(), std::back_inserter(v));
+  return std::inner_product(std::next(v.begin(), 3), v.end(), v.begin(), 0,
+                            std::plus<>(), std::greater<>());
 }
 int main() {
   std::cout << "Part one: " << part_one() << std::endl;
