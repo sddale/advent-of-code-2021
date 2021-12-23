b75fbb519f0a37d22635843a8862ecaf30cdcaa8
sddale <seth.d.dale@gmail.com>
2021-12-23 17:45:16 -0500

    tidying up

--- day08/day08.cpp
+++ day08/day08.cpp
@@ -1,8 +1,4 @@
-#include <algorithm>
-#include <bitset>
-#include <iterator>
-#include <sstream>
-#include <unordered_map>
+#include <numeric>
 
 #include "../aoc.h"
 
@@ -10,6 +6,7 @@
 
 auto part_one() -> const int {
   std::ifstream fin("../input.txt");
+  assert(fin.is_open());
   std::string s;
   int count = 0;
   while (not fin.eof()) {
@@ -27,32 +24,26 @@ auto part_one() -> const int {
 auto part_two() -> const int {
   // bitsets track on/off for each of the seven display segment
   // hash maps are used to correlate segment config to displayed val
-  constexpr std::bitset<7> one{0B0010010};
-  constexpr std::bitset<7> four{0B0111010};
   const std::unordered_map<std::bitset<7>, int> seg_dict = {
-      {0B1110111, 0}, {one, 1},       {0B1011101, 2}, {0B1011011, 3},
-      {four, 4},      {0B1101011, 5}, {0B1101111, 6}, {0B1010010, 7},
+      {0B1110111, 0}, {0B0010010, 1}, {0B1011101, 2}, {0B1011011, 3},
+      {0B0111010, 4}, {0B1101011, 5}, {0B1101111, 6}, {0B1010010, 7},
       {0B1111111, 8}, {0B1111011, 9}};
-  constexpr std::bitset<7> a{0B1000000};
-  constexpr std::bitset<7> b{0B0100000};
-  constexpr std::bitset<7> c{0B0010000};
-  constexpr std::bitset<7> d{0B0001000};
-  constexpr std::bitset<7> e{0B0000100};
-  constexpr std::bitset<7> f{0B0000010};
-  constexpr std::bitset<7> g{0B0000001};
+  constexpr std::bitset<7> a{0B1000000}, b{0B0100000}, c{0B0010000},
+      d{0B0001000}, e{0B0000100}, f{0B0000010}, g{0B0000001};
   std::ifstream fin("../input.txt");
+  assert(fin.is_open());
   std::string s;
   int sum = 0;
   while (getline(fin, s, '|')) {
-    std::unordered_map<std::bitset<7>, std::string> word_cipher;
+    std::string one_pattern, four_pattern;
     std::unordered_map<char, int> symbol_count;
     std::istringstream sin(s);
     for (int i = 0; i < 10; ++i) {
       sin >> s;
       if (s.length() == 2) {
-        word_cipher[one] = s;
+        one_pattern = s;
       } else if (s.length() == 4) {
-        word_cipher[four] = s;
+        four_pattern = s;
       }
       for (const char& c : s) {
         if (c != ' ') {
@@ -74,14 +65,14 @@ auto part_two() -> const int {
           char_cipher[ch] = f;
           break;
         case 8:  // both 'c' & 'a' occur 8 times, use 'one' to decide
-          if (word_cipher[one].find(ch) != std::string::npos) {
+          if (one_pattern.find(ch) != std::string::npos) {
             char_cipher[ch] = c;
           } else {
             char_cipher[ch] = a;
           }
           break;
         case 7:  // both 'd' & 'g' occur 7 times, use 'four' to decide
-          if (word_cipher.at(four).find(ch) != std::string::npos) {
+          if (four_pattern.find(ch) != std::string::npos) {
             char_cipher[ch] = d;
           } else {
             char_cipher[ch] = g;
@@ -92,7 +83,7 @@ auto part_two() -> const int {
       fin >> s;
       std::bitset<7> b;
       for (const char& c : s) {
-        b |= char_cipher[c];  // OR char bits to find displayed number
+        b |= char_cipher.at(c);  // OR char bits to find displayed number
       }
       sum += seg_dict.at(b) * pow(10, i);
     }
